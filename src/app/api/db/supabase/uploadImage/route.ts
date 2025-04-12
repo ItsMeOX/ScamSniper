cfe5069953ab79df1643998a3cdcaad43a38c65b
ItsMeOX <ongxuan1028@gmail.com>
2025-04-12 17:33:57 +0800

    Fix type error

--- src/app/api/db/supabase/uploadImage/route.ts
+++ src/app/api/db/supabase/uploadImage/route.ts
@@ -1,16 +1,24 @@
 import prisma from '@/app/lib/prisma/db';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { imageUrl } = req.body;
-  if (!imageUrl) return res.status(400).json({ error: 'Missing imageUrl' });
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { imageUrl } = body;
+
+  if (!imageUrl) {
+    return NextResponse.json({ message: 'Missing image url' }, { status: 400 });
+  }
 
   try {
     const saved = await prisma.image.create({
       data: { url: imageUrl }, // adjust based on your model
     });
-    res.status(200).json(saved);
+    return NextResponse.json({ saved }, { status: 200 });
   } catch (err) {
-    res.status(500).json({ error: 'DB save failed' });
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message }, { status: 200 });
+    }
+    return NextResponse.json({ message: 'unknown error' }, { status: 200 });
   }
 }
