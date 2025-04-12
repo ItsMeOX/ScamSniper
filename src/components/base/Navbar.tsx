90e78f80cfd3f29bc13a9e645a904928af003982
ItsMeOX <ongxuan1028@gmail.com>
2025-04-13 03:56:00 +0800

    Update navbar for username and profile picture

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -3,9 +3,20 @@
 import Image from 'next/image';
 import styles from './navbar.module.css';
 import { useRouter } from 'next/navigation';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function NavBar() {
   const router = useRouter();
+  const session = useSession();
+  const userName = session.data?.user.name;
+  const userImageUrl = session.data?.user.image_url;
+  console.log(userName);
+  console.log(session);
+  console.log(userImageUrl);
+
+  async function handleSignout() {
+    await signOut({ callbackUrl: '/' });
+  }
 
   return (
     <header className={styles.container}>
@@ -24,14 +35,43 @@ export default function NavBar() {
           <button className={styles.button} onClick={() => router.push('/')}>
             Home
           </button>
-          <button className={styles.button}>Forum</button>
-          <button className={styles.button}>VerifyAI</button>
+          <button
+            className={styles.button}
+            onClick={() => router.push('/forum')}>
+            Forum
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => router.push('/chatbot')}>
+            VerifyAI
+          </button>
         </div>
         <div className={styles.right_box}>
-          <button className={styles.button}>Login</button>
-          <button className={`${styles.button} ${styles.register_button}`}>
-            Register
-          </button>
+          {session.status !== 'authenticated' && (
+            <>
+              <button
+                className={styles.button}
+                onClick={() => router.push('/account/login')}>
+                Login
+              </button>
+              <button
+                className={`${styles.button} ${styles.register_button}`}
+                onClick={() => router.push('/account/register')}>
+                Register
+              </button>
+            </>
+          )}
+          {session.status === 'authenticated' && (
+            <div className={styles.user_box}>
+              <Image
+                src={userImageUrl ?? '/default_profile_picture.png'}
+                alt="profile picture"
+                width={40}
+                height={40}
+              />
+              <button onClick={handleSignout}>{userName}</button>
+            </div>
+          )}
         </div>
       </div>
     </header>
