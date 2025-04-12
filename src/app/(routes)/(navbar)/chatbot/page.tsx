69e612a9c93be7348dd23cccf9d3ad6cea4f180c
ItsMeOX <ongxuan1028@gmail.com>
2025-04-12 14:45:53 +0800

    Fix error

--- src/app/(routes)/(navbar)/chatbot/page.tsx
+++ src/app/(routes)/(navbar)/chatbot/page.tsx
@@ -1,16 +1,16 @@
-import UserInputContainer from "../../../../components/chatbot/UserInputContainer";
-import HistoryTab from "../../../../components/chatbot/HistoryTab";
-import ChatMessageBox from "../../../../components/chatbot/ChatMessageBox";
-import styles from "./chatbot.module.css";
+import UserInputContainer from '../../../../components/chatbot/UserInputContainer';
+import ChatMessageBox from '../../../../components/chatbot/ChatMessageBox';
+import styles from './chatbot.module.css';
+import HistoryTab from '@/components/chatbot/HIstoryTab';
 
 export default function ChatbotPage() {
   return (
-  <div className={styles.container}>
-    <HistoryTab/>
-    <div className={styles.chat_container}>
-        <ChatMessageBox/>
-        <UserInputContainer/> 
+    <div className={styles.container}>
+      <HistoryTab />
+      <div className={styles.chat_container}>
+        <ChatMessageBox />
+        <UserInputContainer />
+      </div>
     </div>
-  </div>
   );
 }
