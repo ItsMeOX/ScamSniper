73273301a76600a0bb1e95a78946a8d5fbd99952
ItsMeOX <ongxuan1028@gmail.com>
2025-04-13 14:56:14 +0800

    Add history tab

---
+++ src/components/chatbot/HistoryTab.tsx
@@ -0,0 +1,32 @@
+import styles from './historytab.module.css';
+
+export default function HistoryTab({
+  historyChats,
+  onSelectChat,
+}: {
+  historyChats: { chat_name: string; chat_id: number }[];
+  onSelectChat: (chat_id: number) => void;
+}) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <h1 className={styles.gradientText}>VerifyAI</h1>
+      </div>
+
+      <div className={styles.chatButton_container}>
+        {historyChats.map((label, idx) => (
+          <button
+            key={idx}
+            className={`${
+              label.chat_name === 'New Chat'
+                ? styles.newChatButton
+                : styles.chatButton
+            }`}
+            onClick={() => onSelectChat(label.chat_id)}>
+            {label.chat_name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
